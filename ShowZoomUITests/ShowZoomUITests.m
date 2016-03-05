8cd9179ad9ee502388295418c0a398876dc8f226
xinmengwuyu17 <xinmengwuyu@qq.com>
2016-03-05 17:09:40 +0800

    Initial Commit

---
+++ ShowZoomUITests/ShowZoomUITests.m
@@ -0,0 +1,40 @@
+//
+//  ShowZoomUITests.m
+//  ShowZoomUITests
+//
+//  Created by Jhwilliam on 3/5/16.
+//  Copyright © 2016 01. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ShowZoomUITests : XCTestCase
+
+@end
+
+@implementation ShowZoomUITests
+
+- (void)setUp {
+    [super setUp];
+    
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+    
+    // In UI tests it is usually best to stop immediately when a failure occurs.
+    self.continueAfterFailure = NO;
+    // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+    [[[XCUIApplication alloc] init] launch];
+    
+    // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // Use recording to get started writing UI tests.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+@end
